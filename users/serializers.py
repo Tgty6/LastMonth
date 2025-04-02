5ce185c0fa17fbf56a0a8317c55432e8f70ddc0c
Bayastan <baastana7@gmail.com>
2025-04-02 22:06:03 +0600

    Сделано 5 ДЗ

--- users/serializers.py
+++ users/serializers.py
@@ -24,22 +24,3 @@ class UserRegisterSerializer(UserBaseSerializer):
         user = CustomUser.objects.create_user(**validated_data, is_active=False)
         user.generate_confirmation_code()
         return user
-
-
-class UserConfirmSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=150)
-    confirmation_code = serializers.CharField(max_length=6)
-
-    def validate(self, data):
-        username = data.get('username')
-        code = data.get('confirmation_code')
-
-        try:
-            user = CustomUser.objects.get(username=username)
-        except CustomUser.DoesNotExist:
-            raise ValidationError({'username': 'User not found!'})
-
-        if user.confirmation_code != code:
-            raise ValidationError({'confirmation_code': 'Invalid confirmation code!'})
-
-        return data
