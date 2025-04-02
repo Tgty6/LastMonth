7d7154f3269a5dfc701c0ea5dab00de56944def0
Bayastan <baastana7@gmail.com>
2025-04-02 23:22:33 +0600

    Сделано  6 ДЗ

--- users/views.py
+++ users/views.py
@@ -2,7 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from users.models import CustomUser
-from .serializers import UserRegisterSerializer, UserAuthSerializer, UserConfirmSerializer
+from .serializers import UserRegisterSerializer, UserAuthSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
@@ -20,15 +21,21 @@ class AuthAPIView(APIView):
         return Response(status=status.HTTP_401_UNAUTHORIZED, data={'error': 'Invalid credentials or user not confirmed'})
 
 
-@api_view(['GET','POST'])
-def registration_api_view(request):
-    serializer = UserRegisterSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
+class RegistrationAPIView(APIView):
+    permission_classes = [AllowAny]
 
-    user = serializer.save()
-    return Response(status=status.HTTP_201_CREATED, data={'user_id': user.id, 'confirmation_code': user.confirmation_code})
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({'user_id': user.id, 'confirmation_code': user.confirmation_code}, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
-def user_detail_api_view(request):
-    user = request.user
-    return Response({'username': user.username, 'is_active': user.is_active})
+
+class UserDetailAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({'username': user.username, 'is_active': user.is_active})
+
+
