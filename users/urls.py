b309f0ace7f48a585a7d882e32db34c332d20724
Bayastan <baastana7@gmail.com>
2025-04-02 23:45:57 +0600

    Сделано 5 ДЗ

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from users import views
 urlpatterns = [
     path('register/', RegistrationAPIView.as_view(), name='register'),
     path('user/', UserDetailAPIView.as_view(), name='user-detail'),
-    path('authorization/', views.AuthAPIView.as_view()),]
+    path('authorization/', views.AuthAPIView.as_view()),
+]
