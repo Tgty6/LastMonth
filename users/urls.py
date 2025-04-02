af6f22ef46993efe73894754461dd8a170bb80eb
Bayastan <baastana7@gmail.com>
2025-04-02 22:01:25 +0600

    Сделано 5 ДЗ

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from users import views
 urlpatterns = [
     path('registration/', views.registration_api_view),
     path('authorization/', views.AuthAPIView.as_view()),
-    path('confirm/', views.confirm_user_view),
+    path('api/v1/confirm/', views.confirm_user_view),
     path('user/', views.user_detail_api_view),
 ]
